ae1c74e9770cb6149fa0654b27d06716c94cbdbb
raksmey <mang.munyraksmey9999@gmail.com>
2026-01-11 21:10:58 +0700

    init service

---
+++ fraud_action_service/src/test/java/com/api/fraudaction/fraud_action_service/FraudActionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fraudaction.fraud_action_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudActionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

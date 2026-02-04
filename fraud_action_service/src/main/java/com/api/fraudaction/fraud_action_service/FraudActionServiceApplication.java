f7e0ebfb5f37fdd63ca120d7bc0c901ffccc4c1b
raksmey <mang.munyraksmey9999@gmail.com>
2026-02-04 22:55:53 +0700

    Revert "feat: finished fraud-action service."
    This reverts commit b2512795ad3f05adf6cd0951e50520581a765466.

--- fraud_action_service/src/main/java/com/api/fraudaction/fraud_action_service/FraudActionServiceApplication.java
+++ fraud_action_service/src/main/java/com/api/fraudaction/fraud_action_service/FraudActionServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FraudActionServiceApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(FraudActionServiceApplication.class, args);
     }
 

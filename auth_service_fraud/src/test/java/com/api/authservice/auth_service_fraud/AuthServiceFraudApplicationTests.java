101c7189f77dc7ff90939c23414bd3258a12035c
FearLessXT <mang.munyraksmey9999@gmail.com>
2026-02-05 19:14:03 +0700

    add auth service

---
+++ auth_service_fraud/src/test/java/com/api/authservice/auth_service_fraud/AuthServiceFraudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.authservice.auth_service_fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceFraudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
